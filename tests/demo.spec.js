95f76334f5adb3f05f32f3c6afc294222aaf0375
bagalumaji <bagalumaji@gmail.com>
2023-11-04 12:57:31 +0530

    added browser launch content

--- tests/demo.spec.js
+++ tests/demo.spec.js
@@ -1,8 +1,14 @@
 import { chromium, test } from "@playwright/test";
 
 test("demo test", async function () {
-  const browser = await chromium.launch();
+  const browser = await chromium.launch({
+    headless: false,
+  });
   const browserContext = await browser.newContext();
   const page = await browserContext.newPage();
   await page.goto("https://ecommerce-playground.lambdatest.io/");
+  await page.hover("(//span[@class='title' and contains(.,'My account')])[2]");
+  await page.click("'Login'"); //span[@class='title' and contains(.,'Login')]");
+  const title = await page.title();
+  console.log(title);
 });
