8cf625823cbfb0cd7245db797fa4814c8f42bfb6
umajibagal <bagalumaji@gmail.com>
2024-08-19 18:54:57 +0530

    new browser context

---
+++ tests/chromium-context.spec.js
@@ -0,0 +1,11 @@
+
+const {test,expect,chromium} = require('@playwright/test')
+
+test('create a chromium context', async ()=>{
+    const  browser = await chromium.launch();
+    const context = await browser.newContext();
+    const page = await context.newPage();
+    await page.goto("https://the-internet.herokuapp.com/windows")
+    const title = await page.title();
+    console.log(title);
+})
