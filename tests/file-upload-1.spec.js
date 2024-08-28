bcc575e71216736c88bd25c8043b2b6e6ce04f29
Umaji Bagal <bagalumaji@gmail.com>
2024-08-28 08:42:11 +0530

    file upload update

--- tests/file-upload-1.spec.js
+++ tests/file-upload-1.spec.js
@@ -3,16 +3,14 @@ test('test file upload-1', async ({page}) => {
     const file1 = "./tests/comands.txt";
     await page.goto('https://the-internet.herokuapp.com/upload');
     await page.locator("#file-upload").setInputFiles(file1);
-   // await page.pause();
 })
 test('test file upload without attribute type=file', async function ({page}) {
     const file1 = "./tests/comands.txt";
     await page.goto('https://the-internet.herokuapp.com/upload')
-    page.on("filechooser", function (filechooser) {
-        filechooser.setFiles(file1);
+    page.on("filechooser", function (fc) {
+        fc.setFiles(file1);
     })
     await page.locator("#drag-drop-upload").click({force: true});
     console.log(await page.title());
-    //await page.pause();
     await page.waitForTimeout(2000);
 })
