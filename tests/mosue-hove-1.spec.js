775651d2aaf15fde488b244b907f99df30060752
Umaji Bagal <bagalumaji@gmail.com>
2024-08-28 08:28:38 +0530

    mouse hover

--- tests/mosue-hove-1.spec.js
+++ tests/mosue-hove-1.spec.js
@@ -2,18 +2,27 @@ const {test, expect} = require('@playwright/test')
 test('mouse hover flipkart test',async function ({page}) {
     await page.goto('https://www.flipkart.com/')
     await page.waitForLoadState();
-    await page.locator("//span[text()='Login']").click();
-    //await page.press("Enter");
-   // await page.reload();
-  // await page.pause();
-   // await page.getByRole('button',{name:'✕'}).click();
+    await page.locator("//span[text()='Login']").click({button:'left'});
     const electronicsMainMenu = await page.locator("//span[text()='Electronics']");
     await electronicsMainMenu.hover();
-   // await page.getByText('Gaming Laptops').hover();
-   // await page.getByRole('link',{name:'Laptop and Desktop'}).hover();
     await page.getByRole('link',{name:'Gaming Laptops'}).click();
     await page.waitForLoadState();
-    const headingGamingLaptops = await page.getByRole('heading',{name:'Gaming Laptops'}).isVisible();
+    const headingGamingLaptops = await page.getByRole('heading',{name:'Gaming Laptops Store'}).isVisible();
+    console.log(headingGamingLaptops);
+    // await expect(headingGamingLaptops).toBeTruthy();
+})
+test('right click test',async function ({page}) {
+    await page.goto('https://the-internet.herokuapp.com/context_menu');
+   // await page.waitForLoadState("load");
+    console.log(await page.title());
+    console.log(page.url());
     await page.pause();
-    await expect(headingGamingLaptops).toBeTruthy();
+    await page.locator("#hot-spot").click({button:"right"});
+    await page.pause();
+})
+test.only('mouser hover test-2',async function ({page}) {
+    await page.goto('https://www.automationexercise.com/')
+    await page.pause();
+    await page.locator("(//h2[text()='Rs. 500']/following-sibling::p[text()='Blue Top'])[1]").hover();
+    await page.pause();
 })
