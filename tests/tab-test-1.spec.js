81009fd84145ff0a0f56e925d1895464f5a775fa
Umaji Bagal <bagalumaji@gmail.com>
2024-09-03 08:01:43 +0530

    tab test

--- tests/tab-test-1.spec.js
+++ tests/tab-test-1.spec.js
@@ -31,3 +31,26 @@ test('tab test - 1',async function(){
 
 })
 
+test.only('tab-test-2',async function({browser}){
+    const context = await browser.newContext();
+    const page = await context.newPage();
+    await page.goto("https://the-internet.herokuapp.com/windows");
+    const clickHereLink = await page.getByText("Click Here");
+    const pageEventPromise = context.waitForEvent('page');
+    await clickHereLink.click();
+    await page.waitForTimeout(3000);
+    //const page2 = await pageEventPromise;
+    //await page2.waitForTimeout(3000);
+    // console.log(await page2.title());
+    const allPages=context.pages();
+    console.log(await allPages[0].title());
+
+
+    await allPages[0].waitForTimeout(3000);
+    console.log(await allPages[1].title());
+
+    // await page2.close();
+    await page.close();
+    await context.close();
+
+})
