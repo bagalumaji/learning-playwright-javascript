764a2072d91149403eeaba62eb6bfc0bb084bb40
Umaji Bagal <bagalumaji@gmail.com>
2024-09-14 14:02:57 +0530

    data from object

---
+++ tests/data-driven-1.spec.js
@@ -0,0 +1,19 @@
+const {test,expect} = require("@playwright/test")
+test.describe("data-driven-1", ()=>{
+    const data = {
+        username:"tomsmith",
+        password:"SuperSecretPassword!",
+        message:"You logged into a secure area!"
+    };
+    test(`test for user : ${data.username}`,async ({page})=>{
+        await page.goto("https://the-internet.herokuapp.com/login");
+        await page.locator("id=username").fill(data.username);
+        await page.locator("id=password").fill(data.password)
+        await page.locator("//button[normalize-space()='Login']").click();
+        await page.waitForLoadState();
+        const msg=`//div[normalize-space(text())="${data.message}"]`;
+        console.log(msg);
+        const isMessageVisible=await page.locator(msg).isVisible();
+        await expect(isMessageVisible).toBeTruthy();
+    })
+})
