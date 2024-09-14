86d701aeb3ab5123e612796e313e30a1a3a7b12e
Umaji Bagal <bagalumaji@gmail.com>
2024-09-14 14:23:48 +0530

    ddt test update

--- tests/data-driven-2.spec.js
+++ tests/data-driven-2.spec.js
@@ -1,8 +1,9 @@
 const {test,expect} = require("@playwright/test")
 import testdata from '../testdata/logintestdata-1.json'
 test.describe("data-driven-1", ()=>{
-    for (let data of testdata) {
-        test(`test for user : ${data.username}`, async ({page}) => {
+    let i=1;
+    for (let  data of testdata) {
+        test(`test for user : ${data.username} of iteration : ${i++}`, async ({page}) => {
             await page.goto("https://the-internet.herokuapp.com/login");
             await page.locator("id=username").fill(data.username);
             await page.locator("id=password").fill(data.password)
