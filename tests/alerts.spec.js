ef4511cb12624fef3f591482ad4f983f400f1ed2
Umaji Bagal <bagalumaji@gmail.com>
2024-09-19 08:02:48 +0530

    alert dismiss test

--- tests/alerts.spec.js
+++ tests/alerts.spec.js
@@ -4,19 +4,25 @@ const {test, expect} = require('@playwright/test');
 test('alert test -1', async function ({page}) {
   
     await page.goto('https://www.lambdatest.com/selenium-playground/javascript-alert-box-demo')
-   console.log('1');
-
-
+   
    page.on('dialog',d=>{
     console.log(d.message());
     d.accept();
    })
-   
-    await page.getByRole('button',{name:'Click Me'}).first().click();
-    
-    alertDialogPromise.accept()
-    console.log(msg);
-    
+    await page.getByRole('button',{name:'Click Me'}).first().click(); 
+    await page.waitForTimeout(3000);
+    await page.close({runBeforeUnload:true});
+})
+
+test.only('dismis alet test',async function({page}){
+    await page.goto("https://www.lambdatest.com/selenium-playground/javascript-alert-box-demo");
+    page.on('dialog',d=>{
+        const msg = d.message();
+        console.log(msg);
+        d.dismiss();
+    })
+
+    await page.getByRole('button',{name:'Click Me'}).nth(1).click(); 
     await page.waitForTimeout(3000);
     await page.close({runBeforeUnload:true});
 })
