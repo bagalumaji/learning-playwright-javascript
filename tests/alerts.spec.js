6575f466ea4834896fe1b6129f466f30f444882b
Umaji Bagal <bagalumaji@gmail.com>
2024-09-19 08:14:04 +0530

    alert prompt test

--- tests/alerts.spec.js
+++ tests/alerts.spec.js
@@ -14,7 +14,7 @@ test('alert test -1', async function ({page}) {
     await page.close({runBeforeUnload:true});
 })
 
-test.only('dismis alet test',async function({page}){
+test('dismis alet test',async function({page}){
     await page.goto("https://www.lambdatest.com/selenium-playground/javascript-alert-box-demo");
     page.on('dialog',d=>{
         const msg = d.message();
@@ -23,6 +23,29 @@ test.only('dismis alet test',async function({page}){
     })
 
     await page.getByRole('button',{name:'Click Me'}).nth(1).click(); 
+    await page.waitForTimeout(3000);
+    await page.close({runBeforeUnload:true});
+})
+
+test.only('dismis prompt test',async function({page}){
+    await page.goto("https://www.lambdatest.com/selenium-playground/javascript-alert-box-demo");
+    const value = 'sayaji'
+    page.on('dialog',d=>{
+        const msg = d.message();
+        const dvalue = d.defaultValue();
+        console.log(msg);
+        console.log(dvalue);
+        d.type()
+        d.accept(value);
+        console.log(d.defaultValue());
+    })
+
+    await page.getByRole('button',{name:'Click Me'}).last().click(); 
+    await page.waitForTimeout(3000);
+    // await expect(page.locator('id=prompt-demo')).toContainText(value)
+    // expect(await page.getByText(`You have entered '${value}' !`)).toBeVisible();
+    expect(await page.locator(`text=You have entered '${value}' !`)).toBeVisible();
+            
     await page.waitForTimeout(3000);
     await page.close({runBeforeUnload:true});
 })
