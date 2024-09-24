ae991b2b1da7b51632d16ef82a8cf941f896f973
Umaji Bagal <bagalumaji@gmail.com>
2024-09-24 08:41:49 +0530

    custom fixture

---
+++ fixtures/pagefixtures.js
@@ -0,0 +1,13 @@
+import {test as baseTest,expect} from '@playwright/test'
+import LoginPage from '../pageobjects/login.page'
+
+ export const test=baseTest.extend({
+    loginPage:async function({page},use){
+        const loginPage=new LoginPage(page);
+        use(loginPage)
+    },
+});
+
+export {expect};
+
+//export {expect} from "@playwright/test";
